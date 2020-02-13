5aba40537b4276040dce2a6de6d64a2d8d304eb8
Matteo <m.alberti@deeplearningitalia.com>
2020-02-13 15:26:26 +0100

    completed unit testing compute forces

--- testing/testing_compute_forces.c
+++ testing/testing_compute_forces.c
@@ -29,16 +29,17 @@
  *100               # output print frequency
  */
 
-int main(argc, char **argv){
-	mdsys_t sys:
-	sys.natoms=atoi(3)
-	sys.mass=atoi(39.948)
-	sys.epsilon=atoi(0.2379)
-	sys.sigma=atoi(3.405)
-	sys.rcut=2.5
-	sys.box=5.1964
-	sys.nsteps=1000
-	sys.dt=5.0
+int main(){
+	mdsys_t sys;
+	sys.natoms=3;
+	sys.mass=39.948;
+	sys.epsilon=0.2379;
+	sys.sigma=3.405;
+	sys.rcut=2.5;
+	sys.box=5.1964;
+	sys.nsteps=1000;
+	sys.dt=5.0;
+	sys.nfi=0;
 
 	/* allocate memory */
 	sys.rx=(double *)malloc(sys.natoms*sizeof(double));
@@ -49,38 +50,43 @@ int main(argc, char **argv){
 	sys.vy=(double *)malloc(sys.natoms*sizeof(double));
 	sys.vz=(double *)malloc(sys.natoms*sizeof(double));
 	
-	sys.fx=(double *)malloc(sys.natoms*sizeof(double));
-	sys.fy=(double *)malloc(sys.natoms*sizeof(double));
-	sys.fz=(double *)malloc(sys.natoms*sizeof(double));
+	sys.fx=(double *)calloc(sys.natoms,sizeof(double));
+	sys.fy=(double *)calloc(sys.natoms,sizeof(double));
+	sys.fz=(double *)calloc(sys.natoms,sizeof(double));
 
-
+	//Set position of atoms - first 2 atoms inside last one outside
 	sys.rx[0]=1.06574058650169;
         sys.ry[0]=-3.33432278188177;	
 	sys.rz[0]=-2.59038677851747;
 
-	sys.vx[0]=
-	sys.vy[0]=
-	sys.vz[0]=
+	sys.vx[1]=1.5643224621482283e-03;
+	sys.vy[1]=4.8497508563925346e-04;
+	sys.vz[1]=-4.3352481732883966e-04;
 
-	sys.rx[1]=4.19613239880890;
-	sys.ry[1]=-4.97120493339350;
-	sys.rz[1]=-4.97120493339350;
+	sys.rx[2]=4.19613239880890;
+	sys.ry[2]=-4.97120493339350;
+	sys.rz[2]=-4.97120493339350;
 
-	sys.vx[1]=
-	sys.vy[1]=
-	sys.vz[1]=
+	//Set velocity of atoms
+	sys.vx[0]=1.5417020528064903e-05;
+	sys.vy[0]=1.7854479210267119e-05;
+	sys.vz[0]=1.0061778727049613e-03;
 
-	sys.rx[2]=6.18023614761844;
-	sys.ry[2]=-14.4087233421704;
-	sys.rz[2]=5.66497231845133;
+	sys.rx[1]=6.18023614761844;
+	sys.ry[1]=-14.4087233421704;
+	sys.rz[1]=5.66497231845133;
 
-	sys.vx[2]=
-	sys.vy[2]=
-	sys.vz[2]=
+	sys.vx[2]=-7.8119282252798756e-04;
+	sys.vy[2]=8.0770620385540233e-04;
+	sys.vz[2]=-1.4741616364769391e-04;
 
-	for (i=0; i<sys.natoms; ++i) {
-		fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+	//Execute 
+	force(&sys);
+
+	for (int i=0; i<sys.natoms; ++i) {
+		printf("Ar  %20.8f %20.8f %20.8f %20.8f\n", sys.fx[i], sys.fy[i], sys.fz[i]);
+	
+	printf("Epot %20.8f\n", sys.epot);	
 	}
-
 }
 
