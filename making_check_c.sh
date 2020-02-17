3a288d4e8d2c66aa116a58eb3d8a772c2fa258c9
Nazarova Natalia <nnazarov@sissa.it>
2020-02-17 18:42:09 +0100

    name of files changed

--- making_check_c.sh
+++ making_check_c.sh
@@ -7,6 +7,6 @@ for i in $(seq 0 2);
         mv ./src/CF_folder/${i}* ./src/
         make clean
         make
-        perf stat -d  -r 3  --output perf_data_${i}.txt  make check_c
+        perf stat -d  -r 3  --output perf_data_c_${i}.txt  make check_c
         mv ./src/${i}*  ./src/CF_folder/
         done
