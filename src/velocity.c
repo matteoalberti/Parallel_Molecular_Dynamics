561316c9f5d7415c21499dfb5faaa9b6cea9acfc
Nazarova Natalia <nnazarov@sissa.it>
2020-02-18 15:08:42 +0100

    updated const

--- src/velocity.c
+++ src/velocity.c
@@ -13,12 +13,13 @@
 void velverlet(mdsys_t *sys)
 {
     int i;
+    double dmvsq2e = 1/mvsq2e;
 
     /* first part: propagate velocities by half and positions by full step */
     for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        sys->vx[i] += 0.5*sys->dt *dmvsq2e * sys->fx[i] / sys->mass;
+        sys->vy[i] += 0.5*sys->dt *dmvsq2e * sys->fy[i] / sys->mass;
+        sys->vz[i] += 0.5*sys->dt *dmvsq2e * sys->fz[i] / sys->mass;
         sys->rx[i] += sys->dt*sys->vx[i];
         sys->ry[i] += sys->dt*sys->vy[i];
         sys->rz[i] += sys->dt*sys->vz[i];
@@ -29,9 +30,9 @@ void velverlet(mdsys_t *sys)
 
     /* second part: propagate velocities by another half step */
     for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        sys->vx[i] += 0.5*sys->dt *dmvsq2e * sys->fx[i] / sys->mass;
+        sys->vy[i] += 0.5*sys->dt *dmvsq2e * sys->fy[i] / sys->mass;
+        sys->vz[i] += 0.5*sys->dt *dmvsq2e * sys->fz[i] / sys->mass;
     }
 }
 
